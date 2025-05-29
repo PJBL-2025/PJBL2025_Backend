9b963c20f7d678c6126e2eb5eac3e0ee7d436da4
algazza <fadhilaswadira72564@gmail.com>
2025-05-29 13:25:20 +0700

    :heavy_minus_sign: remove jest and zod dependencies

--- src/schemas/address.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const addressSchema = z.object({
-  address: z.string().min(2, 'nama minimal 2 karakter'),
-  zip_code: z.number().min(5, 'kode pos minimal 5 karakter'),
-  destination_code: z.string().min(3, 'kode destinasi kota minimal 3 karakter').toUpperCase(),
-  receiver_area: z.string().min(5, 'distrik kode destinasi kota miniamal 5 karakter').toUpperCase(),
-});
--- src/schemas/checkout.schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const checkoutSchema = z.object({
-  status: z.enum(['pending', 'success', 'cancel']).default('pending'),
-  quantity: z.number().min(1, 'jumlah barang harus lebih dari 1'),  
-});
-
